5746302973416a31251389265e001301bb300176
Andressc19 <joseandrescram1005@gmail.com>
2025-08-18 10:43:25 -0500

    chore: update project structure by adding modules for customers, payments, orders, deliveries, and store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CustomersModule } from './customers/customers.module';
+import { PaymentsModule } from './payments/payments.module';
+import { OrdersModule } from './orders/orders.module';
+import { DeliveriesModule } from './deliveries/deliveries.module';
+import { StoreModule } from './store/store.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CustomersModule, PaymentsModule, OrdersModule, DeliveriesModule, StoreModule],
 })
 export class AppModule {}
