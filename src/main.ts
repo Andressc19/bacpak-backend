020ebf9f279d0a45e2be07f43b79787eb1adbfa7
Andrés Camperos <joseandrescram1005@gmail.com>
2025-08-19 07:33:46 -0500

    feat: configure backend cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,20 @@ import "reflect-metadata";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api");
+  app.enableCors({
+    origin: (origin, callback) => {
+      const allowedOrigins = ["http://localhost:5173"];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("CORS blocked:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
